cb093cbf44e8facf8522e235f2514313acd2cb3e
wangtao <wangtao970503@foxmail.com>
2026-06-07 14:07:59 +0800

    增加延迟消息的实现细节

--- rocketmq-basic/src/main/java/com/wangtao/rocketmq/basic/delaymsg/DelayProducer.java
+++ rocketmq-basic/src/main/java/com/wangtao/rocketmq/basic/delaymsg/DelayProducer.java
@@ -17,6 +17,24 @@ public class DelayProducer {
      * RocketMQ不支持任意精度的延时消息, 目前只有18个等级
      * 1s 5s 10s 30s 1m... 10m 20m 30m 1h 2h
      * 完整的等级见org.apache.rocketmq.store.config.MessageStoreConfig.messageDelayLevel
+     *
+     * 生产者发送延迟消息时，其实只是发送到一个内部名为SCHEDULE_TOPIC_XXXX的主题中(到达broker后，由broker来替换的)，
+     * 这个主题有18个队列(分区)，刚好对应延迟消息的18个等级，后台定时任务会扫描这些队列，判断它有没有到期，若到期则投递
+     * 到真正的主题中。
+     * 注意:
+     * 多master broker集群，如brokera、brokerb、brokerc每个都会有18个队列，每个 Broker 都维护自己的延迟队列
+     * 不能设计成共享的，因为消息本来就是按照主题分布在多个队列中，如果消息本身路由到brokerb中，延迟等级设置为1，如果是
+     * 共享的，brokera(1-6)，brokerb(7-12), brokerc(13-18)，此时brokerb就没有延迟等级为1的队列了，没法进行投递。
+     *
+     * 如何判断是否到期:
+     * consumequeue文件每行为20个字节，commitLogOffset(8)、messageSize(4)、tagHashCode(8)
+     * 本来tagHashCode只需要4个字节(java的hashcode方法返回int)，就是因为延迟消息使用了tagHashCode这个部分来
+     * 存储消息到期时间戳，所以给这部分设计成了8个字节。
+     *
+     * 定时任务行为:
+     * 1. 每个队列都对应一个定时任务
+     * 2. 同一延迟等级都发往了同一个队列，这样队列中的消息就是天然有序的(取决于到达broker的时间)，定时任务只需要扫描队头的元素有没有到期。
+     * 若没有到期，则会计算时间直接睡眠消息到期时再执行，而不是周期性扫描(每秒一次)，因此CPU开销会很低。
      */
     public static void main(String[] args) throws Exception{
         // 初始化一个producer并设置Producer group name
